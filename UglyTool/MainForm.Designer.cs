28d1a067dad319c87bfb618b559774085c7fb6e0
Marco van Zoest <marco@primaldev.nl>
2015-03-18 14:19:09 +0100

    Changed to English

--- UglyTool/MainForm.Designer.cs
+++ UglyTool/MainForm.Designer.cs
@@ -69,9 +69,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(16, 8);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Zoek";
+            this.label1.Text = "Search";
             // 
             // searchResultUsers
             // 
@@ -87,36 +87,36 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(16, 62);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.Size = new System.Drawing.Size(67, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Gevonden Gebruikers";
+            this.label2.Text = "Users Found";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(343, 73);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.Size = new System.Drawing.Size(54, 13);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Eigenschappen";
+            this.label3.Text = "Properties";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(25, 398);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.Size = new System.Drawing.Size(60, 13);
             this.label4.TabIndex = 8;
-            this.label4.Text = "Lid van:";
+            this.label4.Text = "Member of:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(354, 397);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.Size = new System.Drawing.Size(53, 13);
             this.label5.TabIndex = 9;
-            this.label5.Text = "Leden:";
+            this.label5.Text = "Members:";
             // 
             // panel1
             // 
@@ -160,9 +160,9 @@
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(13, 199);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(101, 13);
+            this.label6.Size = new System.Drawing.Size(74, 13);
             this.label6.TabIndex = 6;
-            this.label6.Text = "Gevonden Groepen";
+            this.label6.Text = "Groups Found";
             // 
             // SearchResultGroups
             // 
